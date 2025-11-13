c3816d496df9e097c6b04a65425aa8aaa3b16df0
Ayushi Yadav <Ayushi59599@gmail.com>
2025-11-13 20:01:59 +0000

    connect to MongoDB

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient } from "mongodb";
 
 const app = express();
 const PORT = 8000;
+const MONGO_URI = "";
 
-// MIDDLEWARE
 app.use(cors());
 app.use(express.json());
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url} at ${new Date().toISOString()}`);
-  next();
-});
+
+// CONNECT TO MONGODB
+let db;
+async function connectDB() {
+  const client = new MongoClient(MONGO_URI);
+  await client.connect();
+  db = client.db();
+  console.log("MongoDB connected successfully");
+}
 
 app.get("/test", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+connectDB().then(() => {
+  app.listen(PORT, () => console.log(`🚀 Server running at http://localhost:${PORT}`));
 });
