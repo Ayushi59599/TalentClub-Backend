5f06e6128637dc71715f8a2d980e925765f6a753
Ayushi Yadav <Ayushi59599@gmail.com>
2025-11-13 19:59:11 +0000

    add basic middleware

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = 8000;
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url} at ${new Date().toISOString()}`);
+  next();
+});
 
 app.get("/test", (req, res) => {
   res.send("Hello World");
